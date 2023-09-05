f6930cad29bae96c5f486e281f08fa9f597b63f7
David Sanchez <dasbar@amazon.com>
2023-09-05 21:58:17 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "dasbar-aws"
     workspaces {
       name = "hashicat-aws"
