e40e3fa891c1703403b84cb472c7c92ddca4bf82
David Sanchez <dasbar@amazon.com>
2023-09-05 22:13:16 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/dasbar-aws/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix  = "${var.prefix}"
-  acl    = "private"
-
-
 }
